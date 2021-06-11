d104ba40a59c1086c3265ed148c15bf7239c6832
mdmartinvega <mdmartinvega@gmail.com>
2021-06-11 13:28:20 +0200

    Añadir la primera respuesta con json

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,7 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 //que pone a disposición nuestra multitud de caracteristicas
 class DefaultController extends AbstractController
 {
-    
+    const PEOPLE = [
+        ['name' => 'Carlos', 'email' => 'carlos@correo.com', 'age' => 30, 'city' => 'Benalmádena'],
+        ['name' => 'Carmen', 'email' => 'carmen@correo.com', 'age' => 25, 'city' => 'Fuengirola'],
+        ['name' => 'Carmelo', 'email' => 'carmelo@correo.com', 'age' => 35, 'city' => 'Torremolinos'],
+        ['name' => 'Carolina', 'email' => 'carolina@correo.com', 'age' => 38, 'city' => 'Málaga'],
+    ];
+
     /**
      * @Route("/default", name="default_index")
      * La clase ruta debe estar precedida en los comentarios por una @
@@ -42,6 +49,13 @@ class DefaultController extends AbstractController
      */
     public function Saludo(): Response {
         return new Response('<html><body>hola</body></html>');
+    }
+
+     /**
+     * @Route("/json", name="default_index_json")
+     */
+    public function indexJson(): JsonResponse {
+        return new JsonResponse(self::PEOPLE);
     }
 
    
