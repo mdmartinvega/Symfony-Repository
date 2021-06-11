bd63a0a91d4fd3ee47d33d17e03b3246d224bc84
mdmartinvega <mdmartinvega@gmail.com>
2021-06-11 13:19:50 +0200

    Añadir nueva ruta con su accion asociada

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,10 +3,15 @@
 declare(strict_types=1);
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController {
+
+//AbstractController es un controlador de Symfony
+//que pone a disposición nuestra multitud de caracteristicas
+class DefaultController extends AbstractController
+{
     
     /**
      * @Route("/default", name="default_index")
@@ -19,10 +24,25 @@ class DefaultController {
         //Una acción siempre debe devolver una respuesta.
         //Por defecto deberá ser un objeto de la clase
         //Symfony\Component\HttpFoundation\Response;
-        return new Response('Hola');
+        
+        //render() es un método heredado de AbstractControoller
+        //que devuelve el contenido declarado en una plantilla de Twig
+        // https://twig.symfony.com/doc/3.x/templates.html
+
+        //symfony console es un comando equivalente a php bin/console
+
+        $name = 'Loli';
+
+        return $this->render('default/index.html.twig', [
+            'nombre'=>$name
+        ]);   
+    }
+    /**
+     * @Route("/saludo", name="default_saludo")
+     */
+    public function Saludo(): Response {
+        return new Response('<html><body>hola</body></html>');
     }
 
-
-
-
+   
 }
