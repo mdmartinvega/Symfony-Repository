f92ff0cd78eefc50d71e565ffe0ae96e6245403b
mdmartinvega <mdmartinvega@gmail.com>
2021-06-21 12:37:32 +0200

    Crear autenficador para el login con formulario

--- src/Entity/AdminUser.php
+++ src/Entity/AdminUser.php
@@ -52,6 +52,10 @@ class AdminUser implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    public function getUserName() {
+        return $this->getUserIdentifier();
+    }
+
     /**
      * A visual identifier that represents this user.
      *
